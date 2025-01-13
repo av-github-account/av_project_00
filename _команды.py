5f841d7c38de9f4757aa7274d59ca7e8f08c999c
av <av.avramenko@gmail.com>
2025-01-13 12:20:45 +0300

    1st commit

---
+++ _команды.py
@@ -0,0 +1,42 @@
+# Узнать версию питона:
+python3 --version
+
+# Создать окружение venv_av:
+python3 -m venv venv_av 
+
+# Активировать окружение venv_av:
+source venv_av/bin/activate 
+
+# Показать все библиотеки в окружении venv_av:
+pip list
+
+# Инициалиазция poetry в проекте (в папке)
+poetry init
+
+
+poetry add fastapi
+poetry add alembic
+poetry run alembic init migrations
+poetry add ruff
+poetry add mypy
+poetry add dynaconf
+
+
+git init                # инициализация репозитория
+git remote add origin [url]  # добавление удаленного репозитория
+
+
+
+
+
+
+git push -u origin main
+git push
+git push origin +master
+
+git remote -v # просмотр удаленных репозиториев
+
+git branch -m новое_имя_ветки # переименование ветки
+
+def main():
+    print("Hello, world!")
